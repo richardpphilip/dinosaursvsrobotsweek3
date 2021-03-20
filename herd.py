140aa163227936e51d83e963889983795ff969cc
richardpphilip <80114998+richardpphilip@users.noreply.github.com>
2021-03-20 11:00:04 -0700

    made dino attack work on main.  need to hard code integer.  working to make it more dynamic

--- herd.py
+++ herd.py
@@ -7,5 +7,13 @@ class Herd:
     dinosaur_two = Dinosaur()
     dinosaur_three = Dinosaur()
     dinosaur_herd_list = [dinosaur_one, dinosaur_two, dinosaur_three]
-
+    i = 0
+    def dinosaurs_win(self):
+        while i <= len(robot_fleet.robot_fleet_list):
+            dinosaur_herd.dinosaur_herd_list[0].dino_attack(robot_fleet.robot_fleet_list[0])
+            if robot_fleet.robot_fleet_list[0].robot_health <= 0:
+                del robot_fleet.robot_fleet_list[0]
+                i += 1
+        else:
+            print('all robots are dead.  Dinosaurs win!')
 
--- main.py
+++ main.py
@@ -7,13 +7,10 @@ if __name__ == '__main__':
         robot_fleet = Fleet()
         i = 0
 
-        def dinosaurs_win(self):
-            while i <= len(robot_fleet.robot_fleet_list):
-                dinosaur_herd.dinosaur_herd_list[0].dino_attack(robot_fleet.robot_fleet_list[0])
-                if robot_fleet.robot_fleet_list[0].robot_health <= 0:
-                    del robot_fleet.robot_fleet_list[0]
-                    i += 1
-            else:
-                print('all robots are dead.  Dinosaurs win!')
-
-        dinosaur_herd.dinosaurs_win()
+        while i <= 2:
+            dinosaur_herd.dinosaur_herd_list[0].dino_attack(robot_fleet.robot_fleet_list[0])
+            if robot_fleet.robot_fleet_list[0].robot_health <= 0:
+                del robot_fleet.robot_fleet_list[0]
+                i += 1
+        else:
+            print('all robots are dead.  Dinosaurs win!')
